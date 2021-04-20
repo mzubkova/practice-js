bcff90894e0ebcc526cc717375789f630ab0feb8
mzubkova <mary010118@gmail.com>
2021-04-20 14:59:17 +0300

    update tasks

--- index.js
+++ index.js
@@ -2,57 +2,47 @@
 // getDays( 2021 , 4)  //30
 // getDays( 2021 , ‘April’)  //30
 
+function getDays(year, month) {
+  if (typeof year !== "number") {
+    return "Not a number";
+  }
+  var getMonthStr = new Date(Date.parse(month + " 1," + year)).getMonth() + 1;
+  return new Date(year, getMonthStr, 0).getDate();
+}
+
 // 2) Через прототип расширьте встроенный объект Number методом isOdd(),
 // который возвращает true, если число нечетное.
 
-// 3) Написать функцию every, которая будет принимать первым аргументом массив,
-// а вторым функцию колбэк(которая будет принимать каждый элемент массива и возвращать true либо false).
-// Результатом функции должно быть логическое выражение true / false  в зависимости от того, выполняется ли
-// условие для каждого из элементов.
+Number.prototype.isOdd = function (number) {
+  if (this % 2 === 0) {
+    return false;
+  }
+  return true;
+};
+var number = 15;
 
+// 3) Написать функцию every, которая будет принимать первым аргументом массив,
+// а вторым функцию колбэк(которая будет принимать каждый элемент массива и
+// возвращать true либо false).
+// Результатом функции должно быть логическое выражение true / false  в
+// зависимости от того, выполняется ли
+// условие для каждого из элементов.
 // Example:
 // every([8, 2, 4], function (num){
 // 	return  num%2===0
 // })  // true
 
-// Все созданные функции должны быть покрыты тестами!
-
-// function getDaysStr(month, year) {
-//   if (typeof month === "string")
-//     for (var i = 0; i < monthStr.length; i++) {
-//       monthStr[i] = monthOdd;
-//       console.log((monthStr[i] = monthOdd));
-//       return element;
-//     }
-//   return;
-// }
-
-function getDays(year, month) {
-  var monthOdd = 31;
-  var monthEven = 30;
-  var february = 28;
-  var result = "";
-
-  if (typeof (year, month) !== "number") {
-    return "Not a number";
+var every = function (arr, call) {
+  for (var i = 0; i < arr.length; i++) {
+    if (!call(arr[i])) {
+      return false;
+    }
   }
-  switch (true) {
-    case month % 2 === 0 && month !== 2:
-      result += monthEven;
-      break;
-    case month % 2 !== 0:
-      result += monthOdd;
-      break;
-    case month === 2:
-      result += february;
-      break;
-    default:
-      result += "error";
-      break;
-  }
-  return +result;
-}
+  return true;
+};
 
-console.log(getDays(2021, 3));
+every([8, 2, 4], function (number) {
+  return number % 2 === 0;
+});
 
-module.exports = { getDays };
+module.exports = { getDays, every };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-var { getDays } = require("./index");
+var { getDays, every } = require("./index");
 
 describe("test days in month", function () {
   it("should be a function", function () {
@@ -7,16 +7,52 @@ describe("test days in month", function () {
   it("should be defined", function () {
     expect(getDays()).toBeDefined();
   });
-  it("should work with a numeric argument", function () {
-    expect(getDays("1")).toBe("Not a number");
+  it("should work with a numeric first argument", function () {
+    expect(getDays("2021", "April")).toBe("Not a number");
   });
-  it("should work with even month", function () {
-    expect(getDays(2021, 3)).toBe(31);
-  });
-  it("should work with odd month", function () {
+  it("should return number of even month", function () {
     expect(getDays(2021, 4)).toBe(30);
   });
-  it("should work with february month", function () {
-    expect(getDays(2021, 2)).toBe(28);
+  it("should return number of odd month", function () {
+    expect(getDays(2021, 3)).toBe(31);
+  });
+  it("should return number of string month", function () {
+    expect(getDays(2021, "April")).toBe(30);
+  });
+});
+
+describe("test method isOdd", function () {
+  var evenNumber = 2;
+  var oddNumber = 1;
+  it("should be defined", function () {
+    expect(Number.prototype.isOdd).toBeDefined();
+  });
+  it("should be a function", function () {
+    expect(typeof Number.prototype.isOdd).toBe("function");
+  });
+  it("should return false", function () {
+    expect(evenNumber.isOdd()).toBe(false);
+  });
+  it("should return true", function () {
+    expect(oddNumber.isOdd()).toBe(true);
+  });
+});
+
+describe("test every", function () {
+  it("should be defined", function () {
+    expect(every).toBeDefined();
+  });
+  it("should be a function", function () {
+    expect(typeof every).toBe("function");
+  });
+
+  function cb(number) {
+    return number % 2 === 0;
+  }
+  it("should return true", function () {
+    expect(every([8, 2, 4], cb)).toEqual(true);
+  });
+  it("should return false", function () {
+    expect(every([8, 2, 5], cb)).toEqual(false);
   });
 });
